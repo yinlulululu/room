18351765521a2da89360803198f7fdf0e2078ce5
尹露 <yinlu1228@163.com>
2022-04-03 21:20:52 +0800

    feat:echarts

--- miniprogram/pages/find/index.js
+++ miniprogram/pages/find/index.js
@@ -1,96 +1,6 @@
 // pages/find/index.js
 import * as echarts from '../../ec-canvas/echarts';
 
-function initLineChart(canvas, width, height, dpr) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr // new
-  });
-  canvas.setChart(chart);
-
-  var option = {
-    title: {
-      text: '预约日期曲线图',
-      left: 'center'
-    },
-    grid: {
-      containLabel: true,
-      left: 10
-    },
-    tooltip: {
-      show: true,
-      trigger: 'axis'
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['博学楼自习室', '勤学楼自习室', '南教自习室', '生科楼自习室', '东主自习室'],
-      axisLabel: {
-        interval: 0
-      }
-      // show: false
-    },
-    yAxis: {
-      x: 'center',
-      type: 'category',
-      splitLine: {
-        lineStyle: {
-          type: 'dashed'
-        }
-      }
-      // show: false
-    },
-    series: [{
-      name: '预约日期',
-      type: 'line',
-      smooth: true,
-      data: ['1/22', '1/03', '12/21', '2/22', '07/s21']
-    }]
-  };
-  chart.setOption(option);
-  return chart;
-}
-
-function initPieChart(canvas, width, height, dpr) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr // new
-  });
-  canvas.setChart(chart);
-
-  var option = {
-    tooltip: {
-      trigger: 'item'
-    },
-    title: {
-      text: '预约状态饼图',
-      left: 'center'
-    },
-    backgroundColor: "#ffffff",
-    series: [{
-      label: {
-        normal: {
-          fontSize: 14
-        }
-      },
-      type: 'pie',
-      center: ['50%', '50%'],
-      radius: ['20%', '40%'],
-      data: [{
-        value: 55,
-        name: '已预约'
-      }, {
-        value: 20,
-        name: '已取消'
-      }]
-    }]
-  };
-
-  chart.setOption(option);
-  return chart;
-}
 Page({
 
   /**
@@ -100,17 +10,29 @@ Page({
     orderList: [],
     formatter() {},
     ec_line: {
-      onInit: initLineChart
+      lazyLoad: true, // 延迟加载
     },
     ec_pie: {
-      onInit: initPieChart
-    }
+      lazyLoad: true, // 延迟加载
+    },
+    // 图表数据源
+    addressList: [],
+    dateList: [],
+    pieList: [{
+      value: 55,
+      name: '已预约'
+    }, {
+      value: 20,
+      name: '已取消'
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: async function (options) {
+    this.echartsLineComponnet = this.selectComponent('#mychart-dom-line');
+    this.echartsPieComponnet = this.selectComponent('#mychart-dom-pie');
     const that = this
     await this.getMyOrderList()
     this.setData({
@@ -148,12 +70,134 @@ Page({
       .get({
         success: res => {
           wx.hideLoading()
+          this.init_echarts();
+          const _addressList = []
+          const _dateList = []
+          const isOrderList = []
+          const isCancelList = []
+          res.data.forEach((item) => {
+            _addressList.push(item.name)
+            _dateList.push(item.date)
+            if (item.order_status === '0') isOrderList.push(item)
+            if (item.order_status === '1') isCancelList.push(item)
+          })
+          const _formatDateList = []
+          _dateList.forEach((item) => {
+            _formatDateList.push(Number(item.split('年')[1].split('月')[0]) + '/' + Number(item.split('年')[1].split('月')[1].split('日')[0]))
+          })
           this.setData({
             orderList: res.data,
+            addressList: _addressList,
+            dateList: _formatDateList,
+            pieList: [{
+              name: '已预约',
+              value: isOrderList.length
+            }, {
+              name: '已取消',
+              value: isCancelList.length
+            }]
           });
         }
       })
   },
+
+  // 初始化echart
+  init_echarts() {
+    this.echartsLineComponnet.init((canvas, width, height) => {
+      // 初始化图表
+      const Chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      Chart.setOption(this.getLineOption());
+      return Chart;
+    });
+
+    this.echartsPieComponnet.init((canvas, width, height) => {
+      // 初始化图表
+      const Chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      Chart.setOption(this.getPieOption());
+      return Chart;
+    });
+  },
+
+  // 折线图option
+  getLineOption() {
+    const that = this
+    var option = {
+      title: {
+        text: '预约日期曲线图',
+        left: 'center'
+      },
+      grid: {
+        containLabel: true,
+        left: 10
+      },
+      tooltip: {
+        show: true,
+        trigger: 'axis'
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: that.data.addressList,
+        axisLabel: {
+          interval: 0,
+          rotate: "70"
+
+        }
+        // show: false
+      },
+      yAxis: {
+        x: 'center',
+        type: 'category',
+        splitLine: {
+          lineStyle: {
+            type: 'dashed'
+          }
+        }
+        // show: false
+      },
+      series: [{
+        name: '预约日期',
+        type: 'line',
+        smooth: true,
+        data: that.data.dateList
+      }]
+    };
+    return option;
+  },
+
+  // 饼图option
+  getPieOption() {
+    const that = this
+    var option = {
+      tooltip: {
+        trigger: 'item'
+      },
+      title: {
+        text: '预约状态饼图',
+        left: 'center'
+      },
+      backgroundColor: "#ffffff",
+      series: [{
+        label: {
+          normal: {
+            fontSize: 14
+          }
+        },
+        type: 'pie',
+        center: ['50%', '50%'],
+        radius: ['20%', '40%'],
+        data: that.data.pieList
+      }]
+    };
+    return option;
+  },
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
